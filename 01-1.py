2358f28cb23c6c52576c5d93eda1d5267322623b
DariaLaz <daria.lazarova03@gmail.com>
2025-12-07 12:28:40 +0200

    Day 1 - 1

---
+++ 01-1.py
@@ -0,0 +1,18 @@
+from main import run_input
+
+
+def solution(input):
+    score = 50
+    count = 0
+
+    for i in input:
+        sign = 1 if i[0] == 'R' else -1
+        score += (sign * int(i[1:]))
+        score %= 100
+        if score == 0:
+            count += 1
+
+    return count
+
+
+run_input(1, 1, solution)
